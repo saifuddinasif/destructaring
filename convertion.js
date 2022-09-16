f8c523eec6078661fe4f8300d14eab89ecb6d316
My Name <mdarfinji45@gmail.com>
2022-09-16 17:11:08 +0800

    finish

---
+++ convertion.js
@@ -0,0 +1,24 @@
+const num1 =43;
+
+// console.log(num1)
+
+const numstring =num1 + ' '
+
+// console.log(numstring)
+
+
+const input = '560';
+
+const inputNumber = +input;
+
+// console.log(inputNumber);
+
+
+// const isActive =true;
+const isActive =false;
+
+const showUser = () => console.log('display user');
+
+const hideUser = () => console.log("hide user")
+/* if left side is true than right side woll be executed otherwise it will not be executed */
+isActive && showUser();
