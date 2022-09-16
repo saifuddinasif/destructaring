8bacee047f6d50356d581e48ca2f83562aed29a0
My Name <mdarfinji45@gmail.com>
2022-09-16 16:21:55 +0800

    finish

--- destructuring.js
+++ destructuring.js
@@ -5,7 +5,7 @@ const numbers = [34,65]
 // const [x,y] = [34,65]
 const [x,y] = numbers;
 
-console.log(x,y)
+// console.log(x,y)
 
 /* taking two numbers and returning them inside an array */
 
@@ -17,7 +17,41 @@ function boxify(num1,num2){
 
 }
 
-console.log(boxify(90,34));
+// console.log(boxify(90,34));
+
+/* destructuring function  */
+const [first, second ] =boxify(90,34);
 
 
+const student = {
+
+    name :'sakib khan',
+
+    age:32,
+    miovies: ['king khan', ' shoaig ali']
+
+}
+
+/* destructurinfg an array of student object  */
+const[fisrt, seconds ] = student.miovies;
+
+
+const empployee= {
+
+    id:'vs code',
+    designation:'developer',
+    machine:'mac',
+    languages:['html', 'css', 'js'],
+    specification: {
+
+        height: 66,
+        weight:46,
+        adress:seconds,
+
+    }
+}
+
+const {id, designation} = empployee;
+
+const {weight,adress} = empployee?.specification;
 
